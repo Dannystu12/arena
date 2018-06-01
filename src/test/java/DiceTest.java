4b6ecd4edccc909d6941d18711640e333ac8b838
Daniel Stewart <mgdog1@googlemail.com>
2018-06-01 16:07:28 +0100

    Dice class added and tested

---
+++ src/test/java/DiceTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+import utils.Dice;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class DiceTest{
+
+    @Test
+    public void canRollD20(){
+        int roll;
+        for(int i = 0; i < 100; i++){
+            roll = Dice.roll(20);
+            assertTrue( roll <= 20);
+            assertTrue( roll >= 1);
+        }
+    }
+
+    @Test
+    public void canHandleNegativeSides(){
+        assertEquals(0, Dice.roll(0));
+        assertEquals(0, Dice.roll(-5));
+    }
+}
